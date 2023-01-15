9248432924c1172116845a24fd8b1f0e75084fca
Zenky <bzenky@gmail.com>
2023-01-15 15:54:00 -0300

    writable / transform stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable, Writable } from 'node:stream'
+import { Readable, Transform, Writable } from 'node:stream'
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -18,5 +18,22 @@ class OneToHundredStream extends Readable {
     }
 }
 
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(chunk.toString() * 10)
+
+        callback()
+    }
+}
+
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
