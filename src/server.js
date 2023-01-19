6557180fde837903d02550faacd25bf48f4202a7
Zenky <bzenky@gmail.com>
2023-01-19 20:57:37 -0300

    routes with regex

--- src/server.js
+++ src/server.js
@@ -9,10 +9,12 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     const route = routes.find(route => {
-        return route.method === method && route.url === url
+        return route.method === method && route.path.test(url)
     })
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+
         return route.handler(req, res)
     }
 
